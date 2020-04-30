573bab89ac256f7ed32cafb3eb8a0a892c11eb82
Devin Udy <devin.udy@gmail.com>
2020-04-30 12:05:17 -0600

    Created functions

--- index.php
+++ index.php
@@ -1,31 +1,9 @@
 <?php
-    $cars = [
-        [
-            "Model" => "F-150",
-            "Make" => "Ford",
-            "Wheels" => 4,
-            "Doors" => 4
-        ], 
-        [
-            "Model" => "Benz",
-            "Make" => "Mercedes",
-            "Wheels" => 4,
-            "Doors" => 4
-        ], 
-        [
-            "Model" => "Navigator",
-            "Make" => "Lincoln",
-            "Wheels" => 4,
-            "Doors" => 4
-        ], 
-        [
-            "Model" => "BRZ",
-            "Make" => "Subaru",
-            "Wheels" => 4,
-            "Doors" => 2
-        ]
-    ];
-    $cars[0]["Doors"] = 2;    
+      function example($a, $b) {
+          for ($i = 0; $i < $b; $i++) {
+              echo "<h1>{$a} {$i}</h1>";
+          }
+      }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,11 +13,6 @@
     <title>Document</title>
 </head>
 <body>
-    <p>
-        <?php echo $cars[2]["Wheels"];?>
-    </p>
-    <p>
-        <?php echo $cars[0]["Doors"];?>
-    </p>
+    <?php echo example("The count is", 4);?>
 </body>
 </html>
